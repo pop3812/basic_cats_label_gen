7ac7383b59acefa7aa0883f045492285f2644f46
pop3812 <yakwang@kaist.ac.kr>
2012-10-01 16:52:01 -0400

    ENH - Added new function that generates all the image names in subsynset

--- main.m
+++ main.m
@@ -6,7 +6,8 @@
 % Set Synset ID and Label List for all the basic level categories 
 
 % Set download path
-home_folder = '/Users/kwang/Documents/MATLAB/ImageNet/basic_categories';
+home_folder = './basic_categories_test';
+struct_xml = [home_folder, '/structure_released.xml'];
 
 % Set label list and corresponding wnid list
 % gets input list from txt file separated by semi-colon.
@@ -33,19 +34,38 @@ for idx = 1 : n_categories
     disp([char(10), 'Working on basic level category : ', label, '... [', ...
           num2str(idx), ' / ', num2str(n_categories), ']']);
     
-    n_of_category_images = download_single_category(home_folder, wnid, label);
+    sub_list = genRecursiveSynsetList(home_folder, wnid, 1);
+    sub_list = unique(sub_list);
     
     % Save the information about the basic level category images
     basic_level_categories(idx).label = label;
     basic_level_categories(idx).wnid = wnid;
-    basic_level_categories(idx).n_of_images = n_of_category_images;
+    sub_categories = struct([]);
+    n_subcategories = length(sub_list);
+    
+    for sub_idx = 1 : n_subcategories
+        sub_wnid = char(sub_list(sub_idx));
+        [imname, urls] = wnidToImname(sub_wnid);
+        n_of_imgs = length(imname);
+        
+        % Set info about the subcategories
+        sub_categories(sub_idx).wnid = sub_wnid;
+        sub_categories(sub_idx).definition = wnidToDefinition(struct_xml, sub_wnid);
+        sub_categories(sub_idx).imname_list = imname;
+        sub_categories(sub_idx).urls = urls;
+        sub_categories(sub_idx).n_of_imgs = n_of_imgs;
+        
+    end
+    
+    basic_level_categories(idx).sub_categories = sub_categories;
+    
     
     % Backup the results (save every time)
     save([home_folder, '/basic_level_categories_info.mat'], 'basic_level_categories');
     
 end
 
-%% Write a info.txt file
+%% Write a info txt files
 %
 % home_folder/info.txt file includes the information about the n_of_images
 % of each basic level category
